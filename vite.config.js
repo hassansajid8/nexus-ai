9bad50812d1f622199f570acebff1c098bcdbc9e
Hassan Sajid <hsajid.123yt@gmail.com>
2024-04-19 15:36:58 +0530

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-export default {
-    pages: {
-      '/chat': {
-        entry: './chat.html'
-      }
-    }
-  }
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        chat: resolve(__dirname, 'chat/index.html'),
+      },
+    },
+  },
+})
